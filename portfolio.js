e44dd531b5f702749444ee2cbea55da665e6e7ee
V-mandloi <vmandloi852@gmail.com>
2024-01-14 13:40:16 +0530

    Final submition of scroll button correction

--- portfolio.js
+++ portfolio.js
@@ -31,13 +31,15 @@ document.addEventListener('click', () => {
 // smooth scroll js starts
 
 var navMenuAnchorTags = document.querySelectorAll('.navbar a');
-console.log(navMenuAnchorTags);
-
+// console.log(navMenuAnchorTags);
+let targetSection = null;
 for (var i = 0; i < navMenuAnchorTags.length;i++) {
     navMenuAnchorTags[i].addEventListener('click', function(event) {
         event.preventDefault();
         var targetSectionID = this.textContent.trim().toLowerCase();
-        var targetSection = document.getElementById(targetSectionID);
+        targetSection = document.getElementById(targetSectionID);
+    console.log(targetSection);
+
         // console.log(targetSection);
         var interval = setInterval(function() {
         var targetSectionCoordinates = targetSection.getBoundingClientRect();
@@ -46,19 +48,32 @@ for (var i = 0; i < navMenuAnchorTags.length;i++) {
                 clearInterval(interval);
                 return;
             }
-            window.scrollBy(0, 50);
-        }, 20);
+            window.scrollBy(0, 5);
+        }, 0.5);
     });
 }
 
 // smooth scroll js ends
  
+    // window.scrollBy(0, 716);
+    // for (let i = 0; i <= 716; i++){
+    //     console.log(i);
+    //     var j =+ 1;
+    //     var interval = setInterval(function() {
+    //         if(i == 716){
+    //             clearInterval(interval);
+    //             return;
+    //         }
+    //             window.scrollBy(0, 50);
+    //         }, 20);
+
+    // }
 
 // button for next section starts
 
 
 let currentSection = null;
-let currentBottom = null;
+let currentHeight = null;
 var currentSectionId = setInterval(() => {
     const currentScrollPosition = window.scrollY;
 
@@ -68,56 +83,33 @@ const sections = document.querySelectorAll("section");
 // Find the section that is currently in view
 sections.forEach(section => {
   const sectionTop = section.offsetTop;
-//   console.log(sectionTop);
+
   const sectionBottom = sectionTop + section.offsetHeight;
 
   if (currentScrollPosition >= sectionTop && currentScrollPosition <= sectionBottom) {
     currentSection = section.offsetTop;
-    // console.log(currentSection);
-    // console.log(currentSection.offsetHeight);
-    currentBottom = currentSection + 716;
+
+    currentHeight = section.offsetHeight;
   }
 });
-
 // Log the found section to the console
-// console.log(currentBottom);
 }, 1000);
-// console.log(currentBottom);
-var sectionButton = document.getElementById("scroll-button");
-// let targetpoint;
-let locationButton = setInterval(() => {
+
+
+var sectionButton = document.getElementById("scrollButton");
+sectionButton.addEventListener("click", () => {
     var locat = window.scrollY;
-    // console.log(locat);
     let targetpoint = null;
-for(var i = locat; i <= currentSection + 716; i++){
+console.log(currentSection);
+
+for(var i = locat;  i <= currentSection + currentHeight; i++){
     targetpoint++;
 }
-// console.log(targetpoint);
-return (targetpoint);
-}, 100);
-console.log(targetpoint);
-
-
-sectionButton.addEventListener("click", () => {
     window.scrollBy(0, targetpoint);
 
 })
-console.log(sectionButton);
+// console.log(sectionButton);
 
-
-
-/*location for divbutton*/
-var buttonForLoc = document.getElementById("scroll-button");
-// var locationButton = setInterval(() => {
-//     var locat = window.scrollY;
-//     // console.log(locat);
-//     var targetpoint = null;
-// for(var i = locat; i <= currentSection + 716; i++){
-//     targetpoint++;
-// }
-// // console.log(targetpoint);
-// }, 100);
-// button for next section ends
 
 
 
@@ -125,8 +117,20 @@ var buttonForLoc = document.getElementById("scroll-button");
 
 
 $(".mousey").click(function() {
-    window.scrollBy(0, 716);
-})
+
+    var interval = setInterval(function() {
+            var homeSection = document.getElementById("about");
+
+        var targetSectionCoordinates = homeSection.getBoundingClientRect();
+        console.log(targetSectionCoordinates.bottom);
+
+            if(targetSectionCoordinates.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0, 10);
+        }, 5);
+    })
 
 
 // var mouseclick = () => {
@@ -138,3 +142,70 @@ $(".mousey").click(function() {
 //     });
 // }
 // mouse animation click scroll ends
+
+
+
+
+
+
+
+// shweta code
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // sare section ko select kiya &button ko
+//     const sections = document.querySelectorAll("section");
+//     const scrollButton = document.getElementById("scrollButton");
+
+//     let currentSectionIndex = 0;
+//     //   button par listener 
+//     scrollButton.addEventListener("click", function () {
+
+//         // scrollToSection --> mtlb hai ki jo section mila hia use() use hisab se wo smoth scroll krega
+//         scrollToSection(currentSectionIndex);
+//         currentSectionIndex++;
+//         //   console.log(currentSectionIndex);
+//     });
+
+//     window.addEventListener("scroll", function () {
+//         // scrollPosition: kitna neeche scroll kiya hua hai secytion me. 
+//         const scrollPosition = window.scrollY;
+//         /*   function(section, index) -> iteration mein section variable
+//           mein current section ayega, aur index mein us element ka index . */
+//         sections.forEach(function (section, index) {
+//             // sectionTop: kitni doori hai section ka top document ke top se.
+//             const sectionTop = section.offsetTop;
+//             const sectionBottom = sectionTop + section.clientHeight;
+//             /*  agr ye condition true huyi toh wo smj jayega ki hame cureent section me hi
+//              scroll krna hai abhi or agr glt hui toh next section me move kr degi*/
+
+//             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+//                 currentSectionIndex = index;
+//             }
+//         });
+//     });
+
+//     function scrollToSection(index) {
+
+//         // index hame currentSectionIndex se milega kyuki hame use function me call krte time as a parametere diya ahai esliye 
+//         const sectionTop = sections[index].offsetTop;
+//         const sectionHeight = sections[index].clientHeight;
+        
+//         /* sectionTop:  yaani section ka starting point.
+//         windowHeight: Window height, mtlb screen ki height.
+//         sectionHeight: Current section  height.*/
+
+//         //   innerHeight -->"veiwport" screen  height milti hai .
+//         const windowHeight = window.innerHeight;
+
+//         if (sectionHeight < windowHeight) {
+//             // If section height is less than window height, scroll to the center of the section
+//             const scrollTo = sectionTop - (windowHeight - sectionHeight) / 2;
+
+//             // { top: scrollTo, behavior: "smooth" }: Yeh batayega  kitna scroll karna hai, aur kaise karna hai.
+//             window.scrollTo({ top: scrollTo, behavior: "smooth" });
+//         } else {
+//             // If section height is greater than or equal to window height, scroll to the top of the section
+//             window.scrollTo({ top: sectionTop, behavior: "smooth" });
+//         }
+//     }
+// });
